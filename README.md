531aa6e32c0f70d19cde5d6bf97901b7e7dbf311
Heindrick <hcheung01@gmail.com>
2017-10-29 16:27:01 -0700

    final copy

--- README.md
+++ README.md
@@ -7,10 +7,8 @@
 
 ## Instructions
 
-To get started, open `designs.js` and start building out the app's functionality.
+To get started, double click `index.html`
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+1. input row and height
+2. select color
+3. click submit
--- designs.js
+++ designs.js
@@ -1,11 +1,9 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
-
+// declare variables
 var input_rows;
 var input_cols;
 var input_color = "#000000";
+
 
 function makeGrid() {
   var table = document.getElementById("pixel_canvas");
@@ -14,11 +12,14 @@ function makeGrid() {
     table.deleteRow(0);
   input_rows = document.getElementById("input_height").value;
   input_cols = document.getElementById("input_width").value;
+
+  // loop to create input_height and input_width
   for (var i = 0; i < input_rows; i++) {
     var row_elem = table.insertRow(i);
     row_elem.setAttribute("class", "row");
     for (var j = 0; j < input_cols; j++) {
       var cell= row_elem.insertCell(j);
+    //   selecting color for grid boxes
       cell.addEventListener('click', function(evt) {
         evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
         this.style.borderColor="#9ecaed";
@@ -28,7 +29,7 @@ function makeGrid() {
   }
   return false;
 }
-
+// submit button after input height and width plus color
 document.getElementById("sizePicker").addEventListener("submit", function(evt) {
   evt.preventDefault();
   makeGrid();
